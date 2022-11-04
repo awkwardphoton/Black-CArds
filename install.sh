20238b7bf15672222ad02b3d1cf282afd31c1dbf
Ethan Walker <awkwardphoton@gmail.com>
2022-11-04 23:53:02 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+echo "Installing Unzip"
 sudo apt install unzip
+echo "Downloading Project"
 wget https://github.com/awkwardphoton/Black-Cards/archive/refs/heads/main.zip && unzip main.zip && rm main.zip && cd Black-Cards-main
+echo "Copying Fonts"
 cp Fonts/* ~/.local/share/fonts
+echo "Copying Project"
 cp -R "Black Cards" ~/
 echo "Now Enter This Command: cd '~/Black Cards'"
