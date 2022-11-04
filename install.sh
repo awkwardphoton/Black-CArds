d906c552fd88594f9f18896a7109cdfdd7fae2a2
Ethan Walker <awkwardphoton@gmail.com>
2022-11-04 23:47:22 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-sudo apt install unzip -y
-unzip *.zip
-cp Black-Cards-main/Fonts/* ~/.local/share/fonts
+cp /Fonts/* ~/.local/share/fonts
 cp -R "Black-Cards-main/Black Cards" ~/
 echo "Now Enter This Command: cd '~/Black Cards'"
